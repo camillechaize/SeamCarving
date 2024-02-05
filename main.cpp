b09611d7ac5b452d4170813b5265262fc23aa0ae
j3mouad <jmouad2003@gmail.com>
2024-02-05 15:46:57 +0100

    l'ajout de Imagine et l'introduction du syntaxe pour lire les images.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
+#include <Imagine/Images.h>
+#include <Imagine/Graphics.h>
 #include <iostream>
 
+using namespace Imagine;
+
+typedef Image<byte> Img;
 using namespace std;
 
-int main()
-{
-    cout << "Hello rld!" << endl;
+Img Image_energy(Img I){
+    return I;
+}
+
+int main(){
+    Img img;
+    if (!load(img,srcPath("Image_test.jpg"))) // Stop si l'image n'est pas chargee
+        return 0;
+    int const imgw = img.width();
+    int const imgh = img.height();
+    openWindow(imgw, imgh);
+    display(img);
+    endGraphics();
     return 0;
 }
